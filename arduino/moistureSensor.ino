80ad232a0be7a9af176acf55ef9c4c1d587fbd58
0874279 <0874279@hr.nl>
2017-05-24 14:53:03 +0200

    Update with servo and NeoPixel
    Added NeoPixel ring instead of led lights.
    Added servo motor when moist is wet

--- arduino/moistureSensor.ino
+++ arduino/moistureSensor.ino
@@ -1,19 +1,22 @@
-
 /* Connection pins:
 Arduino     Soil Moisture Sensor YL-69
   A0             Analog A0
   5V               VCC
   GND              GND
 */
+#include <Servo.h> 
+#include <Adafruit_NeoPixel.h>
+#define PIN 5
+Servo myservo;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(A0, INPUT); //set up analog pin 0 to be input
-  pinMode(2, OUTPUT); // green led
-  pinMode(3, OUTPUT); // yellow led
-  pinMode(4, OUTPUT); // yellow2 led
-  pinMode(5, OUTPUT); // red led
+  myservo.attach(9);
+
 }
 
 void loop()
@@ -23,39 +26,49 @@ void loop()
   
   if(moist >= 1000) {
    Serial.println("Sensor is not in the Soil or DISCONNECTED");
-   digitalWrite(2, LOW);
-   digitalWrite(3, LOW);
-   digitalWrite(4, LOW);
-   digitalWrite(5, HIGH);//red led gives assumpstion something is wrong 
+   for(int i=0;i<12;i++){
+  strip.begin();
+  strip.setPixelColor(i,255,0,0);
+  strip.show();
+    }
+
   }
   if(moist < 1000 && moist >= 750) { 
    Serial.println("Soil is DRY");
-   digitalWrite(2, LOW);
-   digitalWrite(3, LOW);
-   digitalWrite(4, LOW);
-   digitalWrite(5, HIGH); 
+      for(int i=0;i<12;i++){
+  strip.begin();
+  strip.setPixelColor(i,153,76,0);
+  strip.show();
+    }
+ 
   }
   if(moist < 750 && moist >= 500) {
-   Serial.println("Soil is HUMID"); 
-   digitalWrite(2, LOW);
-   digitalWrite(3, LOW);
-   digitalWrite(4, HIGH);//yellow lights start to turn on to in indicate something good is happening
-   digitalWrite(5, LOW);
+   Serial.println("Soil is HUMID");
+      for(int i=0;i<12;i++){
+  strip.begin();
+  strip.setPixelColor(i,153,153,0);
+  strip.show();
+    } 
+
   }
   if(moist < 500 && moist >= 300) {
    Serial.println("Soil is very HUMID"); 
-   digitalWrite(2, LOW);
-   digitalWrite(3, HIGH);
-   digitalWrite(4, LOW);
-   digitalWrite(5, LOW);
+   for(int i=0;i<12;i++){
+  strip.begin();
+  strip.setPixelColor(i,255,255,0);
+  strip.show();
+    }
   }
   if(moist < 300) {
    Serial.println("Sensor in WATER");
-   digitalWrite(2, HIGH);//the soil is wet, puzzle is completed
-   digitalWrite(3, LOW);
-   digitalWrite(4, LOW);
-   digitalWrite(5, LOW);
-  }
-  delay(50);
-}
+   myservo.write(180);
+      for(int i=0;i<12;i++){
+  strip.begin();
+  strip.setPixelColor(i,0,153,0);
+  strip.show();
+    }
 
+ 
+  }
+  delay(2000);
+}
